39d2b7a41bb87197e5bdf3209b6b8089b7717916
Aduraline <aduraline19065@gmail.com>
2023-12-20 05:17:55 +0000

    PU-004/configure ssh && update github actions to use docker

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -1,15 +1,23 @@
-resource "digitalocean_container_registry" "pocketurl" {
-  name                   = "pocketurl"
+resource "digitalocean_container_registry" "pocketurl-registry" {
+  name                   = "pocketurl-registry"
   subscription_tier_slug = "starter"
 }
 
-resource "digitalocean_droplet" "pocketurl" {
-  name   = "pocketurl"
+variable "ssh_key" {}
+
+resource "digitalocean_ssh_key" "pocketurl-ssh-key" {
+  name       = "pocketurl-ssh-key"
+  public_key = file(var.ssh_key)
+}
+
+resource "digitalocean_droplet" "pocketurl-droplet" {
+  name   = "pocketurl-droplet"
   image  = "docker-18-04"
   region = "lon1"
   size   = "s-1vcpu-1gb"
+  ssh_keys = [digitalocean_ssh_key.pocketurl-ssh-key.fingerprint]
 }
 
 output "droplet_ip" {
-  value = digitalocean_droplet.pocketurl.ipv4_address
+  value = digitalocean_droplet.pocketurl-droplet.ipv4_address
 }
